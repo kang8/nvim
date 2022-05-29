9f3e46b95a8bd130d4762cd11b5d5c24360d5738
kang <yi1115610574@gmail.com>
2022-05-29 23:40:12 +0800

    Use lua config
    - use packer plugin manage

--- init.vim
+++ init.vim
@@ -1,2 +1,4 @@
-" 主要配置写在 core 里
 execute 'source' fnamemodify(expand('<sfile>'), ':h').'/core/core.vim'
+
+" for lua
+lua require('setup')
---
+++ lua/plugins.lua
@@ -0,0 +1,24 @@
+require("packer").startup({
+  function(use)
+    -- Packer can manage itself
+    use "wbthomason/packer.nvim"
+  end,
+  config = {
+    display = {
+      open_fn = function()
+        return require("packer.util").float({ border = "single" })
+      end,
+    },
+  },
+})
+
+-- 每次保存 plugins.lua 自动安装插件
+pcall(
+  vim.cmd,
+  [[
+    augroup packer_user_config
+    autocmd!
+    autocmd WinLeave plugins.lua source <afile> | PackerSync
+    augroup end
+  ]]
+)
