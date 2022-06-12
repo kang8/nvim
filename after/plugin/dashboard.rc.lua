810fdd8c2df9bcf07313cfd342aaea4acb711949
kang <yi1115610574@gmail.com>
2022-06-12 21:20:00 +0800

    Fix dashboard break change

--- after/plugin/dashboard.rc.lua
+++ after/plugin/dashboard.rc.lua
@@ -1,22 +1,58 @@
-if not vim.g.loaded_dashboard then
-  vim.notify("Not found dashboard.nvim")
+local status, dashboard = pcall(require, 'dashboard')
+
+if not status then
+  vim.notify('Not found glepnir/dashboard-nvim')
   return
 end
 
-vim.g.dashboard_default_executive = "telescope"
+dashboard.hide_statusline = false
 
-vim.g.dashboard_custom_section = {
-  a = { description = { "  Projects                    SPC f p" }, command = "Telescope projects" },
-  b = { description = { "  Recently files              SPC f o" }, command = "Telescope oldfiles" },
-  g = { description = { "  Find file           SPC f f / SPC p" }, command = 'Telescope find_files'},
-  h = { description = { "  Find text                   SPC f g" }, command = 'Telescope live_grep'},
-}
-
-vim.g.dashboard_custom_header = {
+dashboard.custom_header = {
   [[ ██╗  ██╗ █████╗ ███╗   ██╗ ██████╗   ]],
   [[ ██║ ██╔╝██╔══██╗████╗  ██║██╔════╝   ]],
   [[ █████╔╝ ███████║██╔██╗ ██║██║  ███╗  ]],
   [[ ██╔═██╗ ██╔══██║██║╚██╗██║██║   ██║  ]],
   [[ ██║  ██╗██║  ██║██║ ╚████║╚██████╔╝  ]],
   [[ ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝   ]],
+  [[                                      ]],
+  [[                                      ]],
+}
+
+dashboard.custom_center = {
+  {
+    icon = '  ',
+    desc = 'New file                            ',
+    shortcut = ':enew',
+    action = 'enew',
+  },
+  {
+    icon = '  ',
+    desc = 'Projects                          ',
+    shortcut = 'SPC f p',
+    action = 'Telescope projects',
+  },
+  {
+    icon = '  ',
+    desc = 'Recently files                    ',
+    shortcut = 'SPC f o',
+    action = 'Telescope oldfiles',
+  },
+  {
+    icon = '  ',
+    desc = 'Find file                 ',
+    shortcut = 'SPC f f / SPC p',
+    action = 'Telescope find_files',
+  },
+  {
+    icon = '  ',
+    desc = 'Find text                         ',
+    shortcut = 'SPC f g',
+    action = 'Telescope live_grep',
+  },
+  {
+    icon = '  ',
+    desc = 'Close neovim                         ',
+    shortcut = ':qa!',
+    action = 'qa!',
+  },
 }
