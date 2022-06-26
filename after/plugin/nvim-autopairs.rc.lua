19591b0459806c7bc0ca0832a4075ce7c39ce9c1
kang <yi1115610574@gmail.com>
2022-06-26 14:32:49 +0800

    Add custom rules for spaces between parentheses

--- after/plugin/nvim-autopairs.rc.lua
+++ after/plugin/nvim-autopairs.rc.lua
@@ -6,3 +6,38 @@ if not status then
 end
 
 autopairs.setup({})
+
+-- add custom rule
+
+local Rule = require('nvim-autopairs.rule')
+
+autopairs.add_rules({
+  Rule(' ', ' '):with_pair(function(opts)
+    local pair = opts.line:sub(opts.col - 1, opts.col)
+    return vim.tbl_contains({ '()', '[]', '{}' }, pair)
+  end),
+  Rule('( ', ' )')
+    :with_pair(function()
+      return false
+    end)
+    :with_move(function(opts)
+      return opts.prev_char:match('.%)') ~= nil
+    end)
+    :use_key(')'),
+  Rule('{ ', ' }')
+    :with_pair(function()
+      return false
+    end)
+    :with_move(function(opts)
+      return opts.prev_char:match('.%}') ~= nil
+    end)
+    :use_key('}'),
+  Rule('[ ', ' ]')
+    :with_pair(function()
+      return false
+    end)
+    :with_move(function(opts)
+      return opts.prev_char:match('.%]') ~= nil
+    end)
+    :use_key(']'),
+})
