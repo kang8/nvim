2514bb79146458df70f1b43e4afc9a78c8652b27
kang <yi1115610574@gmail.com>
2022-07-23 15:32:19 +0800

    Add plug for syntax highlighting for Kitty config files

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -56,6 +56,7 @@ require('packer').startup({
     })
     use({ 'svban/YankAssassin.vim', event = { 'BufRead', 'BufNewFile' } }) -- yank 时不移动光标
     use({ 'andymass/vim-matchup', event = { 'BufRead', 'BufNewFile' } }) -- 增强 %
+    use({ 'fladson/vim-kitty' }) -- syntax highlighting for Kitty config files
 
     --------------------- Git -----------------------------
     use({ 'lewis6991/gitsigns.nvim' })
