f659f85ecae6381b43b687cca3c6435d5023e83f
kang <yi1115610574@gmail.com>
2022-09-21 14:37:26 +0800

    Don't override neovim default keymap

--- plugin/lspsaga.rc.lua
+++ plugin/lspsaga.rc.lua
@@ -15,9 +15,9 @@ local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<C-j>', '<Cmd>Lspsaga diagnostic_jump_next<CR>', opts)
 vim.keymap.set('i', '<C-k>', '<Cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
 vim.keymap.set('n', 'K', '<Cmd>Lspsaga hover_doc<CR>', opts)
-vim.keymap.set('n', 'gk', '<Cmd>Lspsaga lsp_finder<CR>', opts)
+-- vim.keymap.set('n', 'gk', '<Cmd>Lspsaga lsp_finder<CR>', opts)
 vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-vim.keymap.set("n", "<C-]>", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+-- vim.keymap.set("n", "<C-]>", "<cmd>lua vim.lsp.buf.definition()<CR>", opts) -- Maybe specify language
 vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
 vim.keymap.set('n', 'gp', '<Cmd>Lspsaga preview_definition<CR>', opts)
 vim.keymap.set('n', '<leader>rn', '<Cmd>Lspsaga rename<CR>', opts)
