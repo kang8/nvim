bebcadbea4ef786f6a6b2b62313c9a698c4a7dcd
kang <yi1115610574@gmail.com>
2022-12-15 21:46:52 +0800

    Format files

--- lua/colorscheme.lua
+++ lua/colorscheme.lua
@@ -1,8 +1,8 @@
-local status = pcall(require, "tokyonight")
+local status = pcall(require, 'tokyonight')
 
 if not status then
-  vim.notify("Not found folke/tokyonight.nvim")
+  vim.notify('Not found folke/tokyonight.nvim')
   return
 end
 
-vim.cmd[[colorscheme tokyonight-day]]
+vim.cmd([[colorscheme tokyonight-day]])
