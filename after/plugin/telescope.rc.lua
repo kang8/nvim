dff55e154046f94af3caf4f6dae758baf83fcc03
kang <yi1115610574@gmail.com>
2022-12-15 08:55:41 +0800

    Format files

--- after/plugin/telescope.rc.lua
+++ after/plugin/telescope.rc.lua
@@ -1,24 +1,24 @@
-local status, telescope = pcall(require, "telescope")
+local status, telescope = pcall(require, 'telescope')
 
 if not status then
-  vim.notify("Not found telescope.nvim")
+  vim.notify('Not found telescope.nvim')
   return
 end
 
-vim.api.nvim_set_keymap("n", "<leader>p", "<cmd>Telescope find_files<CR>", {noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<CR>", {noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<F15>", "<cmd>Telescope find_files<CR>", {noremap = true, silent = true }) -- F15 is <command-p>
-vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<CR>", {noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<C-S-f>", "<cmd>Telescope live_grep<CR>", {noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<CR>", {noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<CR>", {noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fe", "<cmd>Telescope env<CR>", {noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fo", "<cmd>Telescope oldfiles<CR>", {noremap = true, silent = true })
-vim.api.nvim_set_keymap("n", "<leader>fp", "<cmd>Telescope projects<CR>", {noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>p', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ff', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<F15>', '<cmd>Telescope find_files<CR>', { noremap = true, silent = true }) -- F15 is <command-p>
 
-telescope.setup{
+vim.api.nvim_set_keymap('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-S-f>', '<cmd>Telescope live_grep<CR>', { noremap = true, silent = true })
 
-}
+vim.api.nvim_set_keymap('n', '<leader>fb', '<cmd>Telescope buffers<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fe', '<cmd>Telescope env<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fo', '<cmd>Telescope oldfiles<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fp', '<cmd>Telescope projects<CR>', { noremap = true, silent = true })
+
+telescope.setup({})
 
 -- telescope extensions
-pcall(telescope.load_extension, "env")
+pcall(telescope.load_extension, 'env')
--- plugin/lspconfig.rc.lua
+++ plugin/lspconfig.rc.lua
@@ -1,29 +1,32 @@
 local status, nvim_lsp = pcall(require, 'lspconfig')
 
 if not status then
-  vim.notice("Not found neovim/nvim-lspconfig")
+  vim.notice('Not found neovim/nvim-lspconfig')
   return
 end
 
+-- Language TypeScript
 nvim_lsp.tsserver.setup({
   filetypes = { 'typescript', 'typescriptreact', 'typescript.tsx' },
-  cmd = { 'typescript-language-server', '--stdio' }
+  cmd = { 'typescript-language-server', '--stdio' },
 })
 
+-- Language Lua
 nvim_lsp.sumneko_lua.setup({
   settings = {
     Lua = {
       diagnostics = {
         -- Get the language server to recognize the 'vim' global
-        globals = { 'vim' }
+        globals = { 'vim' },
       },
 
       workspaces = {
         -- Make the serve aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file('', true)
-      }
-    }
-  }
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+    },
+  },
 })
 
+-- Language C
 nvim_lsp.clangd.setup({})
