08ef6b425f19be48c7751dc8977a964400ee965b
kang <yi1115610574@gmail.com>
2022-12-23 13:20:22 +0800

    Sync Bootstrapping setting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,15 +1,19 @@
--- Packer setup {{{3
--- stylua: ignore start
-local packer_bootstrap = false
-local install_path = vim.fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  packer_bootstrap = vim.fn.system({
-    "git", "clone", "--depth", "1",
-    "https://github.com/wbthomason/packer.nvim",
-    install_path,
-  })
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd([[ packadd packer.nvim ]])
+
+    return true
+  end
+
+  return false
 end
--- stylua: ignore end
+
+local packer_bootstrap = ensure_packer()
+
 require('packer').startup({
   function(use)
     -- Packer can manage itself
