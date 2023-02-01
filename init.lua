062bf957b2f5f7d05f5fea57a91751cde6934f96
kang <yi1115610574@gmail.com>
2023-02-01 13:30:07 +0800

    Use `lazy.nvim` to manage plugins

--- init.lua
+++ init.lua
@@ -1,1 +1,1 @@
-print('hello')
+require('config.lazy')
---
+++ lua/config/lazy.lua
@@ -0,0 +1,31 @@
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+  -- stylua: ignore
+  vim.fn.system({ "git", "clone", "--filter=blob:none", "https://github.com/folke/lazy.nvim.git", "--branch=stable", lazypath })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require('lazy').setup({
+  spec = {
+    -- { import = "plugins" },
+  },
+  defaults = {
+    version = false, -- always use the latest git commit
+  },
+  install = { colorscheme = { 'tokyonight', 'habamax' } },
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        'gzip',
+        -- "matchit",
+        -- "matchparen",
+        -- "netrwPlugin",
+        'tarPlugin',
+        'tohtml',
+        'tutor',
+        'zipPlugin',
+      },
+    },
+  },
+})
