c8876c16b89ee47ab988f2d680ca447f71b185d8
kang <yi1115610574@gmail.com>
2023-02-03 17:36:56 +0800

    More detail emacs keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,5 @@
-vim.keymap.set('i', '<C-a>', '<Home>')
+vim.keymap.set({ 'i', 'c' }, '<C-a>', '<Home>')
 vim.keymap.set('i', '<C-e>', '<End>')
 vim.keymap.set('i', '<C-f>', '<Right>')
 vim.keymap.set('i', '<C-b>', '<Left>')
+vim.keymap.set({ 'i', 'c' }, '<C-d>', '<Del>')
