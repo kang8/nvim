2d88e2f371195e8d4e57da1ea4b88b9cef461a79
kang <yi1115610574@gmail.com>
2023-02-03 10:38:24 +0800

    Basic options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,3 +4,7 @@ vim.g.maplocalleader = ' '
 vim.opt.expandtab = true
 vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
+vim.opt.relativenumber = true
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.scrolloff = 3
+vim.opt.sidescrolloff = 6
