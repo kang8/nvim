fe7e3e063746ecd166ea92a5acb0342714076ee0
kang <yi1115610574@gmail.com>
2023-02-04 22:08:30 +0800

    Add Telescope

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,47 @@
+return {
+  {
+    'nvim-telescope/telescope.nvim',
+    cmd = 'Telescope',
+    dependencies = {
+      'debugloop/telescope-undo.nvim',
+    },
+    keys = {
+      { '<leader>,', '<cmd>Telescope buffers show_all_buffers=true<cr>', desc = 'Switch Buffer' },
+      { '<leader>:', '<cmd>Telescope command_history<cr>', desc = 'Command History' },
+
+      { '<leader>/', '<cmd>Telescope live_grep<cr>', desc = 'Find in Files (Grep)' },
+      { '<C-S-f>', '<cmd>Telescope live_grep<cr>', desc = 'Find in Files (Grep)' },
+
+      { '<leader><space>', '<cmd>Telescope find_files<cr>', desc = 'Find Files (root dir)' },
+      { '<F15>', '<cmd>Telescope find_files<cr>', desc = 'Find Files (root dir)' }, -- <command-p>
+      -- search
+      { '<leader>sa', '<cmd>Telescope autocommands<cr>', desc = 'Auto Commands' },
+      { '<leader>sb', '<cmd>Telescope current_buffer_fuzzy_find<cr>', desc = 'Buffer' },
+      { '<leader>sc', '<cmd>Telescope command_history<cr>', desc = 'Command History' },
+      { '<leader>sC', '<cmd>Telescope commands<cr>', desc = 'Commands' },
+      { '<leader>sd', '<cmd>Telescope diagnostics<cr>', desc = 'Diagnostics' },
+      { '<leader>sh', '<cmd>Telescope help_tags<cr>', desc = 'Help Pages' },
+      { '<leader>sH', '<cmd>Telescope highlights<cr>', desc = 'Search Highlight Groups' },
+      { '<leader>sk', '<cmd>Telescope keymaps<cr>', desc = 'Key Maps' },
+      { '<leader>sM', '<cmd>Telescope man_pages<cr>', desc = 'Man Pages' },
+      { '<leader>sm', '<cmd>Telescope marks<cr>', desc = 'Jump to Mark' },
+      { '<leader>so', '<cmd>Telescope vim_options<cr>', desc = 'Options' },
+      { '<leader>sr', '<cmd>Telescope oldfiles<cr>', desc = 'Recent' },
+      { '<leader>su', '<cmd>Telescope undo<cr>', desc = 'Undo' },
+    },
+    opts = {
+      defaults = {
+        layout_strategy = 'vertical',
+        layout_config = {
+          vertical = {
+            prompt_position = 'top',
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      require('telescope').setup(opts)
+      require('telescope').load_extension('undo')
+    end,
+  },
+}
