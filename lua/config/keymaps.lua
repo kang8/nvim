24c4257f133bc2aea8bf1ca3117c0522c89863b8
kang <yi1115610574@gmail.com>
2023-03-29 22:21:12 +0800

    Map `<C-b>` to move 1 character to the left in command line mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,8 +1,8 @@
 vim.keymap.set({ 'i', 'c' }, '<C-a>', '<Home>')
 vim.keymap.set('i', '<C-e>', '<End>')
 vim.keymap.set('i', '<C-f>', '<Right>')
-vim.keymap.set('i', '<C-b>', '<Left>')
 vim.keymap.set({ 'i', 'c' }, '<C-d>', '<Del>')
+vim.keymap.set({ 'i', 'c' }, '<C-b>', '<Left>')
 
 vim.keymap.set('n', '<F16>', '<cmd>bnext<cr>') -- command + ]
 vim.keymap.set('n', '<F17>', '<cmd>bprevious<cr>') -- command + [
