884581b501322fef0360f495d4f379f14eb4b902
kang <yi1115610574@gmail.com>
2023-04-27 12:49:04 +0800

    Add `todo-comments.nvim` in common plugins

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -131,4 +131,16 @@ return {
       require('project_nvim').setup({})
     end,
   },
+  {
+    'folke/todo-comments.nvim',
+    event = { 'BufReadPost', 'BufNewFile' },
+    opts = {
+      signs = false, -- show icons in the signs column
+    },
+    -- stylua: ignore
+    keys = {
+      { "]t", function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+      { "[t", function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+    },
+  },
 }
