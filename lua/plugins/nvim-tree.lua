67425e90e1fedf2a328d8f2c007d9b53db0ec63b
kang <yi1115610574@gmail.com>
2023-05-01 23:40:45 +0800

    Close the nvim-tree when nvim-tree is the last window
    ref: https://github.com/nvim-tree/nvim-tree.lua/wiki/Auto-Close

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -5,6 +5,15 @@ return {
     { '<leader>e', '<cmd>NvimTreeToggle<cr>', desc = 'File Explorer' },
   },
   init = function()
+    vim.api.nvim_create_autocmd('BufEnter', {
+      nested = true,
+      callback = function()
+        if #vim.api.nvim_list_wins() == 1 and require('nvim-tree.utils').is_nvim_tree_buf() then
+          vim.cmd('quit')
+        end
+      end,
+    })
+
     if vim.fn.argc() ~= 1 then
       return
     end
