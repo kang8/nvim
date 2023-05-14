692140e4f3427839e03f78a59b637b3857f1fe28
kang <yi1115610574@gmail.com>
2023-05-15 00:17:01 +0800

    Support http

---
+++ lua/plugins/lang/http.lua
@@ -0,0 +1,52 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == 'table' then
+        vim.list_extend(opts.ensure_installed, { 'json', 'http' })
+      end
+    end,
+  },
+  {
+    'rest-nvim/rest.nvim',
+    opts = function()
+      vim.keymap.set('n', '<leader>rh', '<Plug>RestNvim', { desc = 'http-rest: [R]equest [H]TTP' })
+      vim.keymap.set('n', '<leader>rp', '<Plug>RestNvimPreview', { desc = 'http-rest: [R]equest HTTP [P]review' })
+
+      return {
+        -- Open request results in a horizontal split
+        result_split_horizontal = false,
+        -- Keep the http file buffer above|left when split horizontal|vertical
+        result_split_in_place = true,
+        -- Skip SSL verification, useful for unknown certificates
+        skip_ssl_verification = false,
+        -- Encode URL before making request
+        encode_url = true,
+        -- Highlight request on run
+        highlight = {
+          enabled = true,
+          timeout = 150,
+        },
+        result = {
+          -- toggle showing URL, HTTP info, headers at top the of result window
+          show_url = true,
+          show_http_info = true,
+          show_headers = true,
+          -- executables or functions for formatting response body [optional]
+          -- set them to false if you want to disable them
+          formatters = {
+            json = 'jq',
+            html = function(body)
+              return vim.fn.system({ 'tidy', '-i', '-q', '-' }, body)
+            end,
+          },
+        },
+        -- Jump to request line on run
+        jump_to_request = false,
+        env_file = '.env',
+        custom_dynamic_variables = {},
+        yank_dry_run = true,
+      }
+    end,
+  },
+}
