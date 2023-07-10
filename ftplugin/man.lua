c93346b642ae2203e34649e4d17e30aef309f101
kang <yi1115610574@gmail.com>
2023-07-10 16:46:28 +0800

    Key `b` is backward a words, do not override it

--- ftplugin/man.lua
+++ ftplugin/man.lua
@@ -4,5 +4,3 @@ vim.opt.scrolloff = 999
 
 vim.keymap.set({ 'n', 'x' }, 'd', '<C-d>')
 vim.keymap.set({ 'n', 'x' }, 'u', '<C-u>')
-vim.keymap.set({ 'n', 'x' }, 'f', '<C-f>') -- Will override default mation hehavior, if it interferes with the use, need to be commented
-vim.keymap.set({ 'n', 'x' }, 'b', '<C-b>') -- Will override default mation hehavior, if it interferes with the use, need to be commented
