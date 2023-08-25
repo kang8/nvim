41b69cb83298d0edd1ff395d475651d4459864b6
kang <yi1115610574@gmail.com>
2023-08-25 10:40:08 +0800

    feat(nvim-tree): Implement toggle tree window maximize/minimize by 'A'

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -77,7 +77,15 @@ return {
       vim.keymap.set('n', 'x', api.node.navigate.parent_close, opts('Close Directory'))
       vim.keymap.set('n', '<C-l>', '<cmd>NvimTreeResize +20<cr>', opts('Increase File Tree'))
       vim.keymap.set('n', '<C-h>', '<cmd>NvimTreeResize -20<cr>', opts('Decrease File Tree'))
-      -- PERF: <leader>ta, Resize nvim-tree window maximize, how to store status
+      vim.keymap.set('n', 'A', function()
+        if vim.g.nvim_tree_width == nil then
+          vim.g.nvim_tree_width = vim.api.nvim_win_get_width(0)
+          vim.cmd(':wincmd |')
+        else
+          vim.api.nvim_win_set_width(0, vim.g.nvim_tree_width)
+          vim.g.nvim_tree_width = nil
+        end
+      end, opts('Toggle Tree Window Maximize/Minimize'))
     end,
     ui = {
       confirm = {
