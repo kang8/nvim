17a39a7b81915f5f893c9334a346915be4c7c0d8
kang <yi1115610574@gmail.com>
2023-08-27 10:03:27 +0800

    feat(nvim-tree): Disable window picker when open file

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -63,6 +63,9 @@ return {
     actions = {
       open_file = {
         quit_on_open = true,
+        window_picker = {
+          enable = false,
+        },
       },
     },
     on_attach = function(bufnr)
