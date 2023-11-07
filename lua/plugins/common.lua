2d643873f81cdf2e8dd0b6275a93a84baa9094d5
kang <yi1115610574@gmail.com>
2023-11-07 11:09:21 +0800

    feat(plugin): New plugin: `m4xshen/hardtime.nvim`

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -213,4 +213,18 @@ return {
   {
     'tpope/vim-sleuth',
   },
+  {
+    'm4xshen/hardtime.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    opts = {
+      hints = {
+        ['[dcyvV][ia][%(%)]'] = {
+          message = function(keys)
+            return 'Use ' .. keys:sub(1, 2) .. 'b instead of ' .. keys
+          end,
+          length = 3,
+        },
+      },
+    },
+  },
 }
