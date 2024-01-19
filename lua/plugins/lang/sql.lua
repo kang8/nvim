ec14bcb90508c9ad959736eb26522748b4e80aea
kang <yi1115610574@gmail.com>
2024-01-19 15:58:44 +0800

    feat(plugin): New plugin: `cfmeyers/dbt.nvim`

---
+++ lua/plugins/lang/sql.lua
@@ -0,0 +1,13 @@
+return {
+  'cfmeyers/dbt.nvim',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'rcarriga/nvim-notify',
+  },
+  ft = 'sql',
+  keys = {
+    { 'gd', '<cmd>DBTGoToDefinition<cr>', mode = { 'n' } },
+    { '<c-]>', '<cmd>DBTGoToDefinition<cr>', mode = { 'n' } },
+  },
+}
