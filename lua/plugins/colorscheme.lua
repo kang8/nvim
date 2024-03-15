02ce2cd38abbdaac68f7b696702414cdeba215a2
kang <yi1115610574@gmail.com>
2024-03-15 10:45:32 +0800

    chore(color): Delete 'Comment' and 'GitSignsCurrentLineBlame' custom_highlights cause upstream have been solved

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -12,8 +12,6 @@ return {
         dropbar = { enabled = true, color_mode = true },
       },
       custom_highlights = {
-        Comment = { fg = '#949494' },
-        GitSignsCurrentLineBlame = { fg = '#b7b7b7' },
         MiniTablineCurrent = { link = 'lualine_a_normal' },
         TabLineSel = { link = 'lualine_a_normal' }, -- MiniTablineCurrent default link to TabLineSel
         MiniTablineModifiedCurrent = { link = 'lualine_a_normal' },
