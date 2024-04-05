1267e6916336d55f0da70a279cd6cea53efa3ca6
kang <yi1115610574@gmail.com>
2024-04-05 22:16:34 +0800

    feat(plugin): New plugin: `mfussenegger/nvim-lint` for lint

---
+++ .luacheckrc
@@ -0,0 +1,8 @@
+std = luajit
+
+-- Global objects defined by the C code
+globals = {
+   "vim",
+}
+
+max_line_length = 150
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -209,4 +209,22 @@ return {
       end, { range = true })
     end,
   },
+  {
+    'mfussenegger/nvim-lint',
+    event = { 'BufReadPre', 'BufNewFile' },
+    config = function()
+      local lint = require('lint')
+
+      lint.linters_by_ft = {
+        lua = { 'luacheck' },
+      }
+
+      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+        group = vim.api.nvim_create_augroup('lint', { clear = true }),
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+    end,
+  },
 }
