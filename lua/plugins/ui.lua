9139a703c359053d81c6ae78fdd2dc5c72ee0e0f
kang <yi1115610574@gmail.com>
2024-04-29 10:26:29 +0800

    feat(plugin)!: replace colorizer.lua with nvim-highlight-colors
    Inspired by: https://github.com/ayamir/nvimdots/commit/14aad3fb1eb7f6926330f9ad62acd986b2b99530

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -177,8 +177,21 @@ return {
     end,
   },
   {
-    'NvChad/nvim-colorizer.lua',
-    config = true,
+    'brenoprata10/nvim-highlight-colors',
+    opts = {
+      ---Render style
+      ---@usage 'background'|'foreground'|'virtual'
+      render = 'virtual',
+
+      ---Set virtual symbol (requires render to be set to 'virtual')
+      virtual_symbol = '■',
+
+      ---Highlight named colors, e.g. 'green'
+      enable_named_colors = false,
+
+      ---Highlight tailwind colors, e.g. 'bg-blue-500'
+      enable_tailwind = true,
+    },
   },
   {
     'b0o/incline.nvim',
