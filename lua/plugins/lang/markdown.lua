751b78f0d17033e924a592cc99a342a20330916d
kang <yi1115610574@gmail.com>
2024-10-15 11:32:24 +0800

    Config `render-markdown.nvim`
    1. Disable sign
    2. Change heading display, make it not very conspicuous

--- lua/plugins/lang/markdown.lua
+++ lua/plugins/lang/markdown.lua
@@ -9,6 +9,18 @@ return {
   {
     'MeanderingProgrammer/render-markdown.nvim',
     ft = 'markdown',
-    opts = {},
+    opts = {
+      heading = {
+        -- Width of the heading background:
+        --  block: width of the heading text
+        --  full:  full width of the window
+        -- Can also be a list of the above values in which case the 'level' is used
+        -- to index into the list using a clamp
+        width = 'block',
+      },
+      sign = {
+        enabled = false,
+      },
+    },
   },
 }
