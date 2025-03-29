7e00f42425a380fc3b814fceb06bca263ccd0764
kang <yi1115610574@gmail.com>
2025-03-29 11:16:16 +0800

    feat(plugin): Add `git-rebase-auto-diff` to display diffs during rebase mode.

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -85,4 +85,11 @@ return {
       },
     },
   },
+  {
+    'yutkat/git-rebase-auto-diff.nvim',
+    ft = { 'gitrebase' },
+    config = function()
+      require('git-rebase-auto-diff').setup()
+    end,
+  },
 }
