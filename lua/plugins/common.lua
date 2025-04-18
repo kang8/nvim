ed9bca9d1e794713747c54e837317508624ea0c5
kang <yi1115610574@gmail.com>
2025-04-18 13:46:04 +0800

    Always expand folder

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -258,6 +258,10 @@ return {
       vim.o.statuscolumn =
         '%=%l%#FoldColumn#%{foldlevel(v:lnum) > foldlevel(v:lnum - 1) ? (foldclosed(v:lnum) == -1 ? "▾" : "▸") : " " }%*'
 
+      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+      vim.o.foldlevelstart = 99
+      vim.o.foldenable = true
+
       require('ufo').setup()
     end,
   },
