6ce15caef0a600fa92881442ce2fb34bd91fe865
kang <yi1115610574@gmail.com>
2025-05-14 10:06:28 +0800

    chore(autocmds): Update auto go to last lockmarks algorithm

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,11 +1,11 @@
--- go to last lockmarks when opening a buffer
-vim.api.nvim_create_autocmd('BufReadPost', {
-  callback = function()
-    local mark = vim.api.nvim_buf_get_mark(0, '"')
-    local lcount = vim.api.nvim_buf_line_count(0)
-    if mark[1] > 0 and mark[1] <= lcount then
-      pcall(vim.api.nvim_win_set_cursor, 0, mark)
-    end
+vim.api.nvim_create_autocmd('BufRead', {
+  group = vim.api.nvim_create_augroup('kang', {}),
+  desc = 'Restore last cursor position',
+  callback = function(args)
+    local bufnr = args.buf
+    local pos = vim.api.nvim_buf_get_mark(bufnr, '"')
+    local winid = vim.api.nvim_get_current_win()
+    vim.api.nvim_win_set_cursor(winid, pos)
   end,
 })
 
