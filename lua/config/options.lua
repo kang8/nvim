07e100099949a22a26ed88a1bde8fdbf137ef4f0
kang <yi1115610574@gmail.com>
2025-05-29 16:48:37 +0800

    fix(vscode-neovim): Disable `vim._extui` for vscode

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -27,6 +27,6 @@ vim.diagnostic.config({
   virtual_lines = { current_line = true },
 })
 
-if vim.fn.has('nvim-0.12') == 1 then
+if vim.fn.has('nvim-0.12') == 1 and not vim.g.vscode then
   require('vim._extui').enable({})
 end
