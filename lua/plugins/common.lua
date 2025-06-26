fd6728ce5dc470da77f26500bf664f02b324a34a
kang <yi1115610574@gmail.com>
2025-06-26 10:16:30 +0800

    Try `nvim-origami`, and remove nvim-ufo since it is incompatible with `nvim-origami`

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -253,60 +253,13 @@ return {
   {
     'chrisgrieser/nvim-origami',
     event = 'VeryLazy',
-    opts = {
-      keepFoldsAcrossSessions = false,
-    },
-  },
-  {
-    'kevinhwang91/nvim-ufo',
-    dependencies = 'kevinhwang91/promise-async',
-    config = function()
+    opts = {},
+    init = function()
       vim.o.statuscolumn =
         '%=%l%#FoldColumn#%{foldlevel(v:lnum) > foldlevel(v:lnum - 1) ? (foldclosed(v:lnum) == -1 ? "▾" : "▸") : " " }%*'
 
-      vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-      vim.o.foldlevelstart = 99
-      vim.o.foldenable = true
-
-      require('ufo').setup({
-        fold_virt_text_handler = function(virtText, lnum, endLnum, width, truncate)
-          local newVirtText = {}
-          local suffix = (' 󰁂 %d '):format(endLnum - lnum)
-          local sufWidth = vim.fn.strdisplaywidth(suffix)
-          local targetWidth = width - sufWidth
-          local curWidth = 0
-          for _, chunk in ipairs(virtText) do
-            local chunkText = chunk[1]
-            local chunkWidth = vim.fn.strdisplaywidth(chunkText)
-            if targetWidth > curWidth + chunkWidth then
-              table.insert(newVirtText, chunk)
-            else
-              chunkText = truncate(chunkText, targetWidth - curWidth)
-              local hlGroup = chunk[2]
-              table.insert(newVirtText, { chunkText, hlGroup })
-              chunkWidth = vim.fn.strdisplaywidth(chunkText)
-              -- str width returned from truncate() may less than 2nd argument, need padding
-              if curWidth + chunkWidth < targetWidth then
-                suffix = suffix .. (' '):rep(targetWidth - curWidth - chunkWidth)
-              end
-              break
-            end
-            curWidth = curWidth + chunkWidth
-          end
-          table.insert(newVirtText, { suffix, 'MoreMsg' })
-          return newVirtText
-        end,
-      })
-
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = { 'gitcommit' },
-        callback = function()
-          require('ufo').detach()
-          vim.opt_local.foldenable = false
-          vim.opt_local.foldcolumn = '0'
-          -- vim.o.foldenable = true
-        end,
-      })
+      vim.opt.foldlevel = 99
+      vim.opt.foldlevelstart = 99
     end,
   },
 }
