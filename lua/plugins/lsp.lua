4fdcc295892444ace2b1b17a3bdd2cf3ea2dd627
kang <yi1115610574@gmail.com>
2025-12-08 11:41:26 +0800

    Enable inlay hints globally instead of per-buffer

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -49,7 +49,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
     local client = vim.lsp.get_client_by_id(args.data.client_id)
     assert(client) -- check client is not nil
     if client.server_capabilities.inlayHintProvider then
-      vim.lsp.inlay_hint.enable(true, { bufnr = args.buf })
+      vim.lsp.inlay_hint.enable(true)
     end
 
     vim.lsp.document_color.enable(true, args.buf, { style = 'virtual' })
